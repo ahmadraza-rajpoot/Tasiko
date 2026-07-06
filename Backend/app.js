de911c64e9f16aa6ba932a3f3896898219e86fb3
Ahmad Raza <razarajpoot99598@gmail.com>
2026-07-06 20:07:05 +0500

    implemented custom ApiResponse class for response consistency

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const {asyncWrapper} = require('./src/utils/asyncWrapper')
+const ApiResponse = require("./src/utils/apiRes")
 const app = express()
 
 app.get("/", asyncWrapper((req, res)=>{
     
-    return res.status(200).json({
-        success:true,
-        message:"Working, started new project"
-    })
+    return res.status(200).json(new ApiResponse(200,"working",{name:"ahmad"}))
 }))
 
 app.listen(3000)
