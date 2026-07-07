ad5d79310dbd6dd0419a2d26a21450c980a74546
Ahmad Raza <razarajpoot99598@gmail.com>
2026-07-07 20:17:23 +0500

    again practiced about api response, and revised node internals

--- Backend/src/utils/apiRes.js
+++ Backend/src/utils/apiRes.js
@@ -1,12 +1,12 @@
 class ApiResponse{
-    constructor(statusCode, message="Success", data=null){
-        this.statusCode = statusCode
-        this.message = message
-        this.success = statusCode < 400
-        this.data = data
+    constructor(statusCode, message = "Success", data=null){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode<400;
     }
 }
 
-
-
 module.exports = ApiResponse
+
+
