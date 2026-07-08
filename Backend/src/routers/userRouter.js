dcbc10f835a63a56211a84326462a01f489f8cd5
Ahmad Raza <razarajpoot99598@gmail.com>
2026-07-08 20:54:37 +0500

    getting started with user router

---
+++ Backend/src/routers/userRouter.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const userRouter = express.Router()
+
+//create user profile
+userRouter.post('/')
+
+//get user profile
+userRouter.get('/')
+
+
+module.exports = userRouter;
