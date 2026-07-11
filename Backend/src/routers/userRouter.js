db51b6da1ac244c1ecc8ebad69974d7fc2965c26
Ahmad Raza <razarajpoot99598@gmail.com>
2026-07-11 21:41:01 +0500

    improved the create user api

--- Backend/src/routers/userRouter.js
+++ Backend/src/routers/userRouter.js
@@ -2,19 +2,17 @@ const express = require("express")
 const ApiResponse = require('../utils/apiRes')
 const userRouter = express.Router()
 const User = require('../models/user')
+const {asyncWrapper} = require('../utils/asyncWrapper')
 
 //create user profile
-userRouter.post('/', async (req,res)=>{
-   
-    const firstName = req.body?.firstName?.trim()
-    const lastName = req.body?.lastName?.trim()
-    const email = req.body?.email?.trim().toLowerCase();
-    const password = req.body?.password;
-
+userRouter.post('/',  asyncWrapper( async (req,res)=>{
+    
+    const {firstName,lastName,email,password} = req.body;
+  
     const data = await User.create({firstName,lastName,email,password})
-    console.log(data)
+
     res.status(201).json(new ApiResponse(201,"User has been created",{firstName,lastName,email}))
-})
+}))
 
 //get user profile
 //userRouter.get('/')
